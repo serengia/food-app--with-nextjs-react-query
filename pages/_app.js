69275cc42987bd999e59588b96ad790bc55d8861
James Serengia <jamesserengia@gmail.com>
2023-04-30 12:54:46 +0300

    Configure react query

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import RootLayout from "@/components/RootLayout";
 import { store } from "@/redux/store";
 import "@/styles/globals.scss";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
+  const queryClient = new QueryClient();
   return (
-    <Provider store={store}>
-      <RootLayout>
-        <Component {...pageProps} />
-      </RootLayout>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+      </Provider>
+    </QueryClientProvider>
   );
 }
